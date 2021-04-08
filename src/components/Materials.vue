fa7c1ca491b8fa862f3985668d0291354edd6b8f
C-Dahr <cameronsdahr@gmail.com>
2021-04-08 18:32:47 -0300

    Turned material icons into buttons, and decomposed material card into a component

---
+++ src/components/Materials.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-card color="brown" dark class="elevation-10">
+    <v-card-title>
+      Materials
+    </v-card-title>
+    <v-card-text>
+      Click on a material below to add it to the bag of crafting.
+      <v-row justify="center" align="center">
+        <v-col v-for="(item,index) in materialKey.slice(0,5)" v-bind:key="item.name" cols="2">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text large>
+                <v-img
+                  v-bind:src="require('../assets/pickups/' + item.icon)"
+                  @click="addItem(index+1)"
+                  v-bind="attrs"
+                  v-on="on">
+                </v-img>
+              </v-btn>
+            </template>
+            <span>{{item.name}}</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col v-for="(item,index) in materialKey.slice(5,10)" v-bind:key="item.name" cols="2">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text large>
+                <v-img
+                  v-bind:src="require('../assets/pickups/' + item.icon)"
+                  @click="addItem(index+6)"
+                  v-bind="attrs"
+                  v-on="on">
+                </v-img>
+              </v-btn>
+            </template>
+            <span>{{item.name}}</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col v-for="(item,index) in materialKey.slice(10,15)" v-bind:key="item.name" cols="2">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text large>
+                <v-img
+                  v-bind:src="require('../assets/pickups/' + item.icon)"
+                  @click="addItem(index+11)"
+                  v-bind="attrs"
+                  v-on="on">
+                </v-img>
+              </v-btn>
+            </template>
+            <span>{{item.name}}</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col v-for="(item,index) in materialKey.slice(15,20)" v-bind:key="item.name" cols="2">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text large>
+                <v-img
+                  v-bind:src="require('../assets/pickups/' + item.icon)"
+                  @click="addItem(index+16)"
+                  v-bind="attrs"
+                  v-on="on">
+                </v-img>
+              </v-btn>
+            </template>
+            <span>{{item.name}}</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col v-for="(item,index) in materialKey.slice(20,25)" v-bind:key="item.name" cols="2">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text large>
+                <v-img
+                  v-bind:src="require('../assets/pickups/' + item.icon)"
+                  @click="addItem(index+21)"
+                  v-bind="attrs"
+                  v-on="on">
+                </v-img>
+              </v-btn>
+            </template>
+            <span>{{item.name}}</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'Materials',
+  components: {
+  },
+  props: {
+    materialKey: Array
+  },
+  data: () => ({
+  }),
+  methods: {
+    addItem (id) {
+      this.$emit('add', id)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,119 +1,135 @@
 <template>
   <v-container fill-height fluid>
+    <v-row justify="center" align="center" class="mt-5">
+      <v-col v-for="item in bag" v-bind:key="item.name">
+        <v-img v-bind:src="require('../assets/pickups/' + materialKey[item-1].icon)" :key="item.name" max-width="50"></v-img>
+      </v-col>
+    </v-row>
     <v-row justify="center" align="center">
       <v-col cols=6>
-        <v-card color="brown" dark class="elevation-10">
-          <v-card-title>
-            Welcome to the Bag of Crafting Calculator
-          </v-card-title>
-          <v-card-text>
-            <v-row justify="center" align="center">
-              <v-col v-for="item in bag" :key="item.name">
-                {{item.name}}
-                <v-img src="item.path" max-width="50"></v-img>
-              </v-col>
-            </v-row>
-            <v-row justify="center" align="center">
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Red_Heart.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Soul_Heart.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Black_Heart.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Eternal_Heart.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Gold_Heart.png" max-width="50"></v-img>
-              </v-col>
-            </v-row>
-            <v-row justify="center" align="center">
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Bone_heart.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Rotten_Heart.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Penny.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Nickel.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Dime.png" max-width="50"></v-img>
-              </v-col>
-            </v-row>
-            <v-row justify="center" align="center">
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Lucky_Penny.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Key.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Golden_Key.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Charged_Key.gif" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Bomb.png" max-width="50"></v-img>
-              </v-col>
-            </v-row>
-            <v-row justify="center" align="center">
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Golden_Bomb.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Giga_Bomb.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Micro_Battery.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Lil_Battery.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Mega_Battery.png" max-width="50"></v-img>
-              </v-col>
-            </v-row>
-            <v-row justify="center" align="center">
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/TarotCard.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Pill_Black_White.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Rune1.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Pickup_Dice_Shard_icon.png" max-width="50"></v-img>
-              </v-col>
-              <v-col cols="2">
-                <v-img src="@/assets/pickups/Pickup_Cracked_Key_icon.png" max-width="40"></v-img>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+        <Materials :materialKey="materialKey" @add="addItem"/>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
+import Materials from '@/components/Materials.vue'
 
 export default {
   name: 'Home',
   components: {
+    Materials
   },
   data: () => ({
-    bag: []
-  })
+    bag: [],
+    materialKey: [
+      {
+        name: 'Red Heart',
+        icon: 'Red_Heart.png'
+      },
+      {
+        name: 'Soul Heart',
+        icon: 'Soul_Heart.png'
+      },
+      {
+        name: 'Black Heart',
+        icon: 'Black_Heart.png'
+      },
+      {
+        name: 'Eternal Heart',
+        icon: 'Eternal_Heart.png'
+      },
+      {
+        name: 'Gold Heart',
+        icon: 'Gold_Heart.png'
+      },
+      {
+        name: 'Bone Heart',
+        icon: 'Bone_heart.png'
+      },
+      {
+        name: 'Rotten Heart',
+        icon: 'Rotten_Heart.png'
+      },
+      {
+        name: 'Penny',
+        icon: 'Penny.png'
+      },
+      {
+        name: 'Nickel',
+        icon: 'Nickel.png'
+      },
+      {
+        name: 'Dime',
+        icon: 'Dime.png'
+      },
+      {
+        name: 'Lucky Penny',
+        icon: 'Lucky_Penny.png'
+      },
+      {
+        name: 'Key',
+        icon: 'Key.png'
+      },
+      {
+        name: 'Golden Key',
+        icon: 'Golden_Key.png'
+      },
+      {
+        name: 'Charged Key',
+        icon: 'Charged_Key.gif'
+      },
+      {
+        name: 'Bomb',
+        icon: 'Bomb.png'
+      },
+      {
+        name: 'Golden Bomb',
+        icon: 'Golden_Bomb.png'
+      },
+      {
+        name: 'Giga Bomb',
+        icon: 'Giga_Bomb.png'
+      },
+      {
+        name: 'Micro Battery',
+        icon: 'Micro_Battery.png'
+      },
+      {
+        name: "Lil' Battery",
+        icon: 'Lil_Battery.png'
+      },
+      {
+        name: 'Mega Battery',
+        icon: 'Mega_Battery.png'
+      },
+      {
+        name: 'Card',
+        icon: 'TarotCard.png'
+      },
+      {
+        name: 'Pill',
+        icon: 'Pill_Black_White.png'
+      },
+      {
+        name: 'Rune/Soul Gem',
+        icon: 'Rune1.png'
+      },
+      {
+        name: 'Dice Shard',
+        icon: 'Pickup_Dice_Shard_icon.png'
+      },
+      {
+        name: 'Cracked Key',
+        icon: 'Pickup_Cracked_Key_icon.png'
+      }
+    ]
+  }),
+  methods: {
+    addItem (id) {
+      this.bag.push(id)
+    }
+  }
 }
 </script>
